4c0ae1e5cd17c8bf3fe554732bf596bbf1bede2c
Travis Northrup <thelinuxvorpal@gmail.com>
2018-06-13 19:01:05 -0500

    initial commit

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,10 +1,10 @@
-const blankLine = '  |   |  ';
-const guessLine = '1 |   |  ';
-const bombLine = '  | x |  ';
+const blankLine = '  |   |  '; // Blank Line
+const guessLine = '1 |   |  '; // Guess Line
+const bombLine = '  | x |  '; // Bomb Line
 
 console.log("This is what an empty board would look like:");
-console.log(`${blankLine}\n${blankLine}\n${blankLine}\n`);
+console.log(`${blankLine}\n${blankLine}\n${blankLine}\n`); // Print Blank Request
 
 
 console.log("This is what a board with a guess and a bomb on it would look like:");
-console.log(`${guessLine}\n${bombLine}\n${blankLine}\n`);
+console.log(`${guessLine}\n${bombLine}\n${blankLine}\n`); // Print Submitted Request
